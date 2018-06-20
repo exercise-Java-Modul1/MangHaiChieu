476d1c6708f501a2751d452f3477d14fd1feb154
1234567890 <phantiensy195@gmail.com>
2018-06-20 21:12:19 +0700

    TinhTongCacSoNamTrenDuongCheoTrongMangHaiChieu

--- src/TinhTongCacSoNamTrenDuongCheo.java
+++ src/TinhTongCacSoNamTrenDuongCheo.java
@@ -4,24 +4,23 @@ public class TinhTongCacSoNamTrenDuongCheo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Bạn muốn mảng của bạn cao bao nhiêu: ");
-        int x = sc.nextInt();
-        System.out.print("Bạn muốn mảng của bạn rộng bao nhiêu: ");
-        int y = sc.nextInt();
-        int[][] hinhvuong = new  int[x][y];
-        for (int i = 0; i < x; i++) {
+        int n = sc.nextInt();
+        int[][] hinhvuong = new  int[n][n];
+        for (int i = 0; i < n; i++) {
             System.out.println("Nhập dữ liệu cho các mảng: ");
-            for (int j = 0; j < y; j++) {
+            for (int j = 0; j < n; j++) {
                 int number = sc.nextInt();
                 hinhvuong[i][j] = number;
             }
         }
-        int tổng = hinhvuong[0][0];
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
-                tổng = tổng + hinhvuong[i][j];
+        int tổng = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i == n - j - 1 || j == n - i - 1) {
+                    tổng = tổng + hinhvuong[i][j];
+                }
             }
         }
-        int tổng_all = tổng - hinhvuong[0][0];
-        System.out.println(tổng_all);
+        System.out.println("Tổng của các số nằm trên đường chéo của hình vuông là: "+tổng);
     }
 }
